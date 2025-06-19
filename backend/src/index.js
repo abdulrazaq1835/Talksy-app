7f66b26578e26c3f6f7bb961de4e697ed25814ce
abdulrazaq1835 <abdulrazzak1835@gmail.com>
2025-06-19 10:12:27 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,11 @@ import { ConnectDB } from "./lib/db.js";
 
 import { app, server } from "./lib/socket.js";
 
+import path from "path"
+
 dotenv.config();
 const PORT = process.env.PORT;
-
+const __dirname =  path.resolve()
 
 
 app.use(
@@ -26,6 +28,15 @@ app.use(cookieparser());
 app.use("/api/auth", authRoutes);
 
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+  app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend","dist","index.html")
+    )
+  })
+}
 //
 server.listen(PORT, () => {
   console.log("My server is running on port: " + PORT);
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+// const BASE_URL="http://localhost:5001/api"
 
 export const axiosInstance = axios.create({
-  baseURL:  "http://localhost:5001/api" ,
+  baseURL:import.meta.env.MODE === "development" ? "http://localhost:5001/api":"/api",
   withCredentials: true,
 });
